942365a71f2eddee021d1ecf13a210b22241d2ad
Mike <mikesantia.6297@gmail.com>
2019-12-21 17:22:32 +0700

    [FEATURE] Category Reducer

--- src/modules/categories/reducer/CategoryReducer.js
+++ src/modules/categories/reducer/CategoryReducer.js
@@ -1,10 +1,20 @@
-import React, {Component} from 'react';
-
-class CategoryReducer extends Component {
-    render() {
-        return (
-        );
+const initialState = {
+    category: [],
+    categoryForm: {
+        categoryName: ""
     }
+
 }
 
-export default CategoryReducer;
+export default function categoryReducer(state = initialState, action) {
+    console.log("categoryReducer", state, action);
+    switch (action.type) {
+        case "FETCH_CATEGORY_SUCCESS":
+            return {...state, category: action.payload};
+        case "HANDLE_CATEGORY_NAME":
+            return {...state, categoryForm: {...state.categoryForm, categoryName: action.categoryName}}
+        default:
+            return {...state}
+    }
+
+}
