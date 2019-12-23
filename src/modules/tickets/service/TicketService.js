c86ce5edb9e590baec2186451dbc38c239ec8fe7
Mike <mikesantia.6297@gmail.com>
2019-12-23 15:55:02 +0700

    [IMPROVE][FEATURE] ticket-form admin

--- src/modules/tickets/service/TicketService.js
+++ src/modules/tickets/service/TicketService.js
@@ -1,14 +1,45 @@
-import React, {Component} from 'react';
+import Swal from "sweetalert2";
 
-class TicketService extends Component {
-    render() {
-        return (
-            < div >
-            
-            < /div>
-    )
-        ;
-    }
+export async function saveDataTicket(event) {
+    const data = await fetch("http://localhost:9090/ticket", {method:"POST",
+        headers:{'Content-Type': 'application/json'},
+        body: JSON.stringify(event)})
+        .then((response) => {
+            if (response.status===200){
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'success',
+                    title: 'Data has been saved',
+                    showConfirmButton: false,
+                    timer: 3000
+                })
+                return response.json();
+            } else if (response.status===500){
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Sorry!!!',
+                    timer:3000
+                })
+            }
+        }).catch(reason => {
+        })
+    return data;
 }
 
-export default TicketService;
+export async function fetchDataTicket() {
+    const data=await fetch(`http://localhost:9090/tickets`,
+        {method:"GET"}).then((response)=>{
+        return response.json()
+    });
+    console.log(data);
+    return data;
+}
+
+export async function fetchDataTicketById(id) {
+    const data=await fetch(`http://localhost:9090/ticket/{id}`,
+        {method:"GET"}).then((response)=>{
+        return response.json()
+    });
+    console.log(data);
+    return data;
+}
