fcd5cf9a0618fdbbbef05fe1cbfdd6e1d9a8b05d
fatiri <ilhamfatiriblog@gmail.com>
2019-12-26 21:32:10 +0700

    [FEATURE][IMPROVE] create Registration Action for make action to reducer

---
+++ src/modules/register/RegistartionAction.js
@@ -0,0 +1,5 @@
+export const registrationFUllName = {type: 'SUCCESS_INPUT_FULL_NAME'}
+export const registrationUsername = {type: 'SUCCESS_INPUT_USER_NAME'}
+export const registrationPassword = {type: 'SUCCESS_INPUT_PASSWORD'}
+export const registrationBornPlace = {type: 'SUCCESS_INPUT_BORN_PLACE'}
+export const registrationBirthDate = {type: 'SUCCESS_INPUT_BIRTH_DATE'}
